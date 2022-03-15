0e0ac177c34b559084c735f1d6451baed2be8afe
Simon-CHOU <mrsimonzhou@gmail.com>
2022-03-15 21:48:35 +0800

    guru: observer design pattern(comment)

--- src/com/simon/design/pattern/observer/Demo.java
+++ src/com/simon/design/pattern/observer/Demo.java
@@ -1,5 +1,9 @@
 package com.simon.design.pattern.observer;
 
+/**
+ * 每当 编辑器 （Editor）对象改变时，它都会通知其订阅者。邮件通知监听器（Email Notification Listener）
+ * 和 日志开启监听器（Log Open Listener）都将通过执行其基本行为来对这些通知做出反应。
+ */
 public class Demo {
     public static void main(String[] args) {
         Editor editor =new Editor();
--- src/com/simon/design/pattern/observer/Editor.java
+++ src/com/simon/design/pattern/observer/Editor.java
@@ -3,11 +3,11 @@ package com.simon.design.pattern.observer;
 import java.io.File;
 
 public class Editor {
-    public EventManager events;
+    public EventManager events;//订阅者类不与编辑器类相耦合， 且能在需要时在其他应用中复用。
     private File file;
 
     public Editor() {
-        this.events = new EventManager("open","save");
+        this.events = new EventManager("open","save");//不改变编辑器代码的情况下添加新的订阅者类型。
     }
 
     public void openFile(String filePath) {
