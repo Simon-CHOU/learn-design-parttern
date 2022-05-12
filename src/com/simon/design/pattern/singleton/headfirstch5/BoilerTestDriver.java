d97eb91986ee17fbb1ba1220ad3de7adcea80713
Simon-CHOU <mrsimonzhou@gmail.com>
2022-05-12 17:07:12 +0800

    headfirst: test singleton in multi-thread case

--- src/com/simon/design/pattern/singleton/headfirstch5/BoilerTestDriver.java
+++ src/com/simon/design/pattern/singleton/headfirstch5/BoilerTestDriver.java
@@ -5,20 +5,29 @@ import java.util.Set;
 
 public class BoilerTestDriver {
     public static void main(String[] args) {
-        Set<Integer> instanceHashCodeSet = new HashSet<>();
         Set<ChocolateBoilerSingleton> instanceHashCodeSet1 = new HashSet<>();
-        Runnable runnable = () -> {
+        Set<ChocolateBoiler> instanceHashCodeSet2 = new HashSet<>();
+        Runnable runnableSingleton = () -> {
             ChocolateBoilerSingleton boiler = ChocolateBoilerSingleton.getInstance();
             int i = boiler.hashCode();
-            instanceHashCodeSet.add(i);
             instanceHashCodeSet1.add(boiler);
             System.out.println(i);
         };
+        Runnable runnableNonSingle = () -> {
+            ChocolateBoiler boiler = new ChocolateBoiler();
+            int i = boiler.hashCode();
+            instanceHashCodeSet2.add(boiler);
+            System.out.println(i);
+        };
         for (int i = 0; i < 1000; i++) {
-            new Thread(runnable).start();
+            new Thread(runnableSingleton).start();
+        }
+        for (int i = 0; i < 1000; i++) {
+            new Thread(runnableNonSingle).start();
         }
 
-        System.out.println("==== sum ===\n" + instanceHashCodeSet1.size() + " instance has been created");
+        System.out.println("==== sum ===\nsingleton:" + instanceHashCodeSet1.size() + " instance has been created\n"
+                + "non-singleton: " + instanceHashCodeSet2.size() + " instance has been created");
         // 疑问，为什么size不是1，单例模式失败了？
     }
 }
